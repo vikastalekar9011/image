f836aa9fc35e95032ca6c6e7194e0323232a689d
Suraj Pinjan <surajbpinjan92@gmail.com>
2019-04-21 14:51:05 +0530

    socket listner fix

--- src/app/list-farmer/services/farmer-list.service.ts
+++ src/app/list-farmer/services/farmer-list.service.ts
@@ -12,16 +12,23 @@ export class FarmerListService {
   farmers: ListUser;
 
   constructor(private socket: Socket, private dataService: FarmerListDataService) {
-    this.socket.on('farmer_saved', (data: any) => { //client knows from server somebody saved farmer
-      this.getList(); //update your list
-    });
-
     this.getList().subscribe((data: any) => {
       if (data.status === 'success') {
         this.farmers = <ListUser> data.payload;
       }
     }
     );
+    
+    this.socket.on('farmer_saved', (data: any) => { //client knows from server somebody saved farmer
+      this.getList().subscribe((data: any) => {
+        if (data.status === 'success') {
+          this.farmers = <ListUser> data.payload;
+        }
+      }
+      )
+    });
+
+
 
   }
 
