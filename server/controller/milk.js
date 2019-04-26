b9a24d5c88c529e037838bdd6fdffbd0a9b43bf0
EC2 Default User <ec2-user@ip-172-31-18-120.us-west-2.compute.internal>
2019-04-26 17:13:16 +0000

    hot fix

--- server/controller/milk.js
+++ server/controller/milk.js
@@ -15,8 +15,18 @@ exports.getAll = (req, res, next) => {
 }
 
 exports.create = (req, res, next) => {
-  // console.log("add milk");
-  Milk.create(req.body, (err, data) => {
+
+var milk=new Milk(req.body);
+console.log(req.body);
+
+milk.validate(function(err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('pass validate');
+});
+
+Milk.create(req.body,(err, data) => {
     if (err){
       console.log('error in add new milk data:::'+JSON.stringify(err));
       return exception.raiseError(req, res, next, "M002", 500, 'Internal Server Error : in milk data');
